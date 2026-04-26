d263a9873829ef4f8ad61bc5ca66bd711293c488
Nesserine Sediri <contact@omanaia.com>
2026-04-26 19:28:37 +0200

    feat: 14 days trial

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -10,13 +10,14 @@ export async function POST(req: NextRequest) {
     const { priceId, email } = await req.json()
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      mode: 'subscription',
-      customer_email: email,
-      line_items: [{ price: priceId, quantity: 1 }],
-      success_url: `${process.env.NEXT_PUBLIC_APP_URL}/subscribe/success`,
-      cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/subscribe`,
-    })
+  payment_method_types: ['card'],
+  mode: 'subscription',
+  customer_email: email,
+  line_items: [{ price: priceId, quantity: 1 }],
+  trial_period_days: 14,
+  success_url: `https://novae-by-omanaia.com/subscribe/success`,
+  cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/subscribe`,
+})
 
     return NextResponse.json({ url: session.url })
   } catch (error: any) {
